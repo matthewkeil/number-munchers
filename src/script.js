7b74a1ed1721b7b84b2d5406eae308a2c87cd838
Matthew Keil <malibuss67@gmail.com>
2019-01-30 10:04:00 -0500

    board turned to class and working on muncher resize event

--- src/script.js
+++ src/script.js
@@ -3,63 +3,78 @@ let _extraLives = 3;
 const _numRows = 5;
 const _numCols = 6
 
-function buildCell(rowIndex, colIndex) {
-    
-    const cell = document.createElement('div')
-    
-    cell.id = `cell-${rowIndex}-${colIndex}`;
-    cell.className = "cell";
+class Board {
 
-    return cell;
-}
+    constructor() {
+        
+        this._element = $('#board');
+        this._rows = _numRows;
+        this._cols = _numCols;
 
-function buildRow(numCols, rowIndex) {
-    
-    const row = document.createElement('div');
-    row.className = "row";
-    row.id = `row-${rowIndex}`;
-
-    for (let i = 0; i < numCols; i++)
-        row.appendChild(buildCell(rowIndex, i));
-    
-    return row;
-}
-
-function displayExtraLives() {
-    for (let i = 0; i < _extraLives; i++) {
-        $('.extra-lives').append('<svg class="number-munch" viewBox="0 0 353.612 255.545" xml:space="preserve"><polygon fill="#DBDBDB" points="86.845,0 86.845,42.579 129.424,42.579 129.424,31.936 108.135,31.936 108.135,10.644 129.424,10.644 129.424,0  "/><rect x="108.135" y="10.644" fill="#1D1D1B" width="21.289" height="21.292"/><polygon fill="#DBDBDB" points="150.387,0 150.387,42.579 192.966,42.579 192.966,31.936 171.677,31.936 171.677,10.644 192.966,10.644 192.966,0"/><rect x="171.677" y="10.644" fill="#1D1D1B" width="21.289" height="21.292"/><polygon class="body" fill="#00FF00" points="246.224,129.199 246.224,85.89 213.755,85.89 213.755,106.201 32.81,106.201 32.81,74.068 64.85,74.068 64.85,96.052 180.95,96.052 180.95,42.579 1.354,42.579 1.354,129.199 85.908,129.199 85.908,245.737 0,245.737 0,255.545 96.393,255.545 96.393,129.199 149.831,129.199 149.831,255.545 246.224,255.545 246.224,245.737 160.315,245.737 160.315,129.199 "/></svg>')
+        for (let i = 0; i < this._rows; i++) {
+            this._element.append(this.buildRow(this._cols, i));
+        }
+        // displayExtraLives();
     }
-}
 
-function buildBoard() {
-    for (let i = 0; i < _numRows; i++) {
-        $('#board').append(buildRow(_numCols, i));
+    buildCell(rowIndex, colIndex) {
+        
+        const cell = document.createElement('div')
+        
+        cell.id = `cell-${rowIndex}-${colIndex}`;
+        cell.className = "cell";
+        cell.innerHTML = `<h3>${3}</h3>`
+    
+        return cell;
     }
-    // displayExtraLives();
+
+
+    buildRow(numCols, rowIndex) {
+        
+        const row = document.createElement('div');
+        row.className = "row";
+        row.id = `row-${rowIndex}`;
+    
+        for (let i = 0; i < numCols; i++)
+            row.appendChild(this.buildCell(rowIndex, i));
+        
+        return row;
+    }
+    
+    displayExtraLives() {
+        for (let i = 0; i < _extraLives; i++) {
+            $('.extra-lives').append('<svg class="number-munch" viewBox="0 0 353.612 255.545" xml:space="preserve"><polygon fill="#DBDBDB" points="86.845,0 86.845,42.579 129.424,42.579 129.424,31.936 108.135,31.936 108.135,10.644 129.424,10.644 129.424,0  "/><rect x="108.135" y="10.644" fill="#1D1D1B" width="21.289" height="21.292"/><polygon fill="#DBDBDB" points="150.387,0 150.387,42.579 192.966,42.579 192.966,31.936 171.677,31.936 171.677,10.644 192.966,10.644 192.966,0"/><rect x="171.677" y="10.644" fill="#1D1D1B" width="21.289" height="21.292"/><polygon class="body" fill="#00FF00" points="246.224,129.199 246.224,85.89 213.755,85.89 213.755,106.201 32.81,106.201 32.81,74.068 64.85,74.068 64.85,96.052 180.95,96.052 180.95,42.579 1.354,42.579 1.354,129.199 85.908,129.199 85.908,245.737 0,245.737 0,255.545 96.393,255.545 96.393,129.199 149.831,129.199 149.831,255.545 246.224,255.545 246.224,245.737 160.315,245.737 160.315,129.199 "/></svg>')
+        }
+    }
 }
 
 class Muncher {
 
     constructor() {
-        
-        this.board = $('#board');
-        
-        this.element = this.board
+        this._board = $('#board');
+
+        this.element = this._board
             .append(this._element)
             .find('#muncher');
     }
 
     get width() {
-        return this.board.find(`#cell-${this.row}-${this.column}`).outerWidth();
+        return this._board.find(`#cell-${this.row}-${this.column}`).outerWidth();
     }
     
-    set width(width) {}
-
-    get height() {
-        return this.board.find(`#cell-${this.row}-${this.column}`).outerHeight();
+    set width(width) {
+        this.element.css('width', width);
+        return width;
     }
 
-    set height(height) {}
+    get height() {
+        return this._board.find(`#cell-${this.row}-${this.column}`).outerHeight();
+    }
+
+    set height(height) {
+        this.element.css('height', height);
+        return height;
+    }
 
     get column() {
         if (!this.element) return 0;
@@ -93,34 +108,41 @@ class Muncher {
         height:${this.height}px;
         top:${this.column}px;
         left:${this.row}"></div>`
-    } 
+    }
+
+    resize() {
+        this.width = this.width;
+        this.height = this.height;
+    }
 
     moveLeft() {
         let col = this.column;
-        this.column = col - 1;
+        this.column = --col < 1 ? 1 : col;
     }
     
     moveRight() {
         let col = this.column;
-        this.column = col + 1;
+        this.column = col < _numCols ? ++col : col;
     }
 
     moveDown() {
         let row = this.row;
-        this.row = row + 1;
+        this.row = row < _numRows ? ++row : row;
     }
     
     moveUp() {
         let row = this.row;
-        this.row = row - 1;
+        this.row = --row < 1 ? 1 : row;
     }
     
     munch() {
-        console.log('nom nom');
+        let cell = `#cell-${this.row - 1}-${this.column - 1 }`
+        console.log(this._board.find(cell + ' h3').text())
+        this._board.find(cell).html('');
     }
 }
 
-buildBoard();
+new Board;
 
 let muncher = new Muncher;
 
