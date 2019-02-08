cdf22b6b63aa760892a6c1a5eecdb40608191242
Matthew Keil <malibuss67@gmail.com>
2019-02-08 13:39:01 -0500

    added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
+const ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
     mode: 'production',
     entry: './src/script.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: path.resolve(__dirname, 'assets')
+        publicPath: path.resolve(__dirname, '/')
     },
     resolve: {
         extensions: ['.js', '.css'],
@@ -17,5 +19,10 @@ module.exports = {
         alias: {
             jquery: "jquery/src/jquery"
         }
+    },
+    devtool: ENV === 'production' ? 'none' : 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'assets'),
+        port: 3000
     }
 }
